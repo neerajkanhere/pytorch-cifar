c7a5c2f37c09831bc9cd90fe150f993f1c25a710
Neeraj Kanhere <neeraj@omnibond.com>
2024-08-22 20:17:01 -0400

    openvino test

--- cpp-inference/build.sh
+++ cpp-inference/build.sh
@@ -1,1 +1,8 @@
-g++ -O3 -ggdb -I/opt/z/tmc3/libs/include/onnxruntime/core/session/ `pkg-config --cflags opencv4` main.cpp `pkg-config --libs opencv4` -lonnxruntime -o test
+g++ -O3 -ggdb \
+-I/opt/z/tmc3/libs/include/onnxruntime/core/session/ \
+-I/opt/z/tmc3/libs/include/onnxruntime \
+`pkg-config --cflags opencv4` \
+main.cpp \
+`pkg-config --libs opencv4` \
+-lonnxruntime -lonnxruntime_providers_shared \
+-o test
--- cpp-inference/main.cpp
+++ cpp-inference/main.cpp
@@ -1,3 +1,5 @@
+#define OPENVINO
+
 #include <chrono>
 #include <cmath>
 #include <exception>
@@ -5,7 +7,11 @@
 #include <iostream>
 #include <limits>
 #include <numeric>
-#include <onnxruntime_cxx_api.h>
+#include <core/session/onnxruntime_cxx_api.h>
+#ifdef OPENVINO
+#include <core/providers/openvino/openvino_provider_factory.h>
+#endif
+
 #include <opencv2/dnn/dnn.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
@@ -53,10 +59,24 @@ int main(int argc, char* argv[]) {
   opts.SetInterOpNumThreads(1);
   opts.SetIntraOpNumThreads(1);
   opts.SetExecutionMode(ORT_SEQUENTIAL);
+  
+  #ifdef OPENVINO
+  std::unordered_map<std::string, std::string> openvino_options;
+    openvino_options["device_type"] = "CPU";
+    openvino_options["precision"] = "FP32";
+    openvino_options["num_of_threads"] = "1";
+    openvino_options["num_of_streams"] = "1";
+    openvino_options["cache_dir"] = "./";
+    opts.AppendExecutionProvider("OpenVINO", openvino_options);
+    opts.SetGraphOptimizationLevel(ORT_DISABLE_ALL);
+    std::cout << "[INFO] Using OpenVINO execution provider" << std::endl;
+  #endif
+
   cv::setNumThreads(1);
 
   Ort::Env env;
   Ort::Session session{env, "./model.onnx", opts};
+
   cv::dnn::Net net = cv::dnn::readNet("./model.onnx");
 
   static constexpr const int width = 32;
@@ -76,10 +96,14 @@ int main(int argc, char* argv[]) {
   int N = 1000;
   std::cout << "======== ONNX ========\n";
   const char* input_names[] = {"input.1"};
-  const char* output_names[] ={"255"}; // mobilenetv1{"255"} mobilenetv2{"518"};
+  const char* output_names[] = {"255"};  // mobilenetv1{"255"} mobilenetv2{"518"};
   auto memory_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
   auto output_tensor = Ort::Value::CreateTensor<float>(memory_info, output_onnx.data(), output_onnx.size(),
                                                        output_shape_.data(), output_shape_.size());
+
+  auto input_tensor = Ort::Value::CreateTensor<float>(memory_info, input_blob.ptr<float>(), input_blob.total(),
+                                                      input_shape.data(), input_shape.size());
+  session.Run(run_options, input_names, &input_tensor, 1, output_names, &output_tensor, 1);
 
   high_resolution_clock::time_point t1 = high_resolution_clock::now();
   for (int i = 0; i < N; i++) {
